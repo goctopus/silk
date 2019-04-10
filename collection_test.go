cc5bae4738d616a261022d5b79446cbd94fe2955
cg33 <chg80333@163.com>
2019-04-10 16:03:37 +0800

    change collection Number to decimal.Decimal

--- collection_test.go
+++ collection_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestCollection_Avg(t *testing.T) {
 	a := []int{1,2,3,4,5,6,6,7,8,8}
-	assert.Equal(t, Collect(a).Sum().ToInt64(), int64(50))
-	assert.Equal(t, Collect(a).Avg().ToFloat64(), float64(5))
+	assert.Equal(t, Collect(a).Sum().IntPart(), int64(50))
+	f, _ := Collect(a).Avg().Float64()
+	assert.Equal(t, f, float64(5))
 
 	b := []map[string]interface{}{
 		{
@@ -22,20 +23,12 @@ func TestCollection_Avg(t *testing.T) {
 		},
 	}
 
-	assert.Equal(t, Collect(b).Sum("foo").ToInt64(), int64(100))
-}
-
-func TestNumber_Add(t *testing.T) {
-	a := NewNumberFromInt64(5)
-	b := NewNumberFromInt64(5)
-	c := NewNumberFromInt64(3)
-
-	assert.Equal(t, a.Add(b).Reduce(c).ToInt64(), int64(7))
+	assert.Equal(t, Collect(b).Sum("foo").IntPart(), int64(100))
 }
 
 func TestCollection_Max(t *testing.T) {
 	a := []int{1,2,3,4,5,6,6,7,8,8}
-	assert.Equal(t, Collect(a).Max().ToInt64(), int64(8))
+	assert.Equal(t, Collect(a).Max().IntPart(), int64(8))
 
 	b := []map[string]interface{}{
 		{
@@ -49,12 +42,12 @@ func TestCollection_Max(t *testing.T) {
 		},
 	}
 
-	assert.Equal(t, Collect(b).Max("foo").ToInt64(), int64(40))
+	assert.Equal(t, Collect(b).Max("foo").IntPart(), int64(40))
 }
 
 func TestCollection_Min(t *testing.T) {
 	a := []int{1,2,3,4,5,6,6,7,8,8}
-	assert.Equal(t, Collect(a).Min().ToInt64(), int64(1))
+	assert.Equal(t, Collect(a).Min().IntPart(), int64(1))
 
 	b := []map[string]interface{}{
 		{
@@ -68,5 +61,5 @@ func TestCollection_Min(t *testing.T) {
 		},
 	}
 
-	assert.Equal(t, Collect(b).Min("foo").ToInt64(), int64(10))
+	assert.Equal(t, Collect(b).Min("foo").IntPart(), int64(10))
 }
