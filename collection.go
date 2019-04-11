5f27c3673eefb74139edc132493056902aff429d
cg33 <chg80333@163.com>
2019-04-11 11:55:48 +0800

    change collection to struct

--- collection.go
+++ collection.go
@@ -1,51 +1,78 @@
 package silk
 
-import "github.com/shopspring/decimal"
+import (
+	"github.com/shopspring/decimal"
+)
 
 // interface{} must be one type of:
-// decimal.Decimal, string, map[string]interface{}
-type Collection []interface{}
+// []decimal.Decimal, []string, []map[string]interface{}, map[string]interface{}
+type Collection struct {
+	value  interface{}
+	length int
+}
 
 func Collect(src interface{}) Collection {
 	var c Collection
 
 	switch src.(type) {
 	case []map[string]interface{}:
-		for _, v := range src.([]map[string]interface{}) {
-			c = append(c, v)
-		}
+		c.value = src
+		c.length = len(src.([]map[string]interface{}))
 	case []string:
-		for _, v := range src.([]string) {
-			c = append(c, v)
-		}
+		c.value = src
+		c.length = len(src.([]string))
+	case map[string]interface{}:
+		c.value = src
+		c.length = len(src.(map[string]interface{}))
 	case []int:
+		var d = make([]decimal.Decimal, 0)
 		for _, v := range src.([]int) {
-			c = append(c, decimal.New(int64(v), 0))
+			d = append(d, decimal.New(int64(v), 0))
 		}
+		c.value = d
+		c.length = len(src.([]int))
 	case []int8:
+		var d = make([]decimal.Decimal, 0)
 		for _, v := range src.([]int8) {
-			c = append(c, decimal.New(int64(v), 0))
+			d = append(d, decimal.New(int64(v), 0))
 		}
+		c.value = d
+		c.length = len(src.([]int8))
 	case []int16:
+		var d = make([]decimal.Decimal, 0)
 		for _, v := range src.([]int16) {
-			c = append(c, decimal.New(int64(v), 0))
+			d = append(d, decimal.New(int64(v), 0))
 		}
+		c.value = d
+		c.length = len(src.([]int16))
 	case []int32:
+		var d = make([]decimal.Decimal, 0)
 		for _, v := range src.([]int32) {
-			c = append(c, decimal.New(int64(v), 0))
+			d = append(d, decimal.New(int64(v), 0))
 		}
+		c.value = d
+		c.length = len(src.([]int32))
 	case []int64:
+		var d = make([]decimal.Decimal, 0)
 		for _, v := range src.([]int64) {
-			c = append(c, decimal.New(v, 0))
+			d = append(d, decimal.New(v, 0))
 		}
+		c.value = d
+		c.length = len(src.([]int64))
 	case []float32:
+		var f = make([]decimal.Decimal, 0)
 		for _, v := range src.([]float32) {
-			c = append(c, decimal.NewFromFloat32(v))
+			f = append(f, decimal.NewFromFloat32(v))
 		}
+		c.value = f
+		c.length = len(src.([]float32))
 	case []float64:
+		var f = make([]decimal.Decimal, 0)
 		for _, v := range src.([]float64) {
-			c = append(c, decimal.NewFromFloat(v))
+			f = append(f, decimal.NewFromFloat(v))
 		}
+		c.value = f
+		c.length = len(src.([]float64))
 	default:
 		panic("wrong type")
 	}
@@ -54,32 +81,26 @@ func Collect(src interface{}) Collection {
 }
 
 func (c Collection) All() []interface{} {
-	return []interface{}(c)
+	panic("implement it")
 }
 
 func (c Collection) Avg(key ...string) decimal.Decimal {
-	return c.Sum(key...).Div(decimal.New(int64(len(c)), 0))
+	return c.Sum(key...).Div(decimal.New(int64(c.length), 0))
 }
 
 func (c Collection) Sum(key ...string) decimal.Decimal {
 	var sum = decimal.New(0, 0)
 
 	if len(key) == 0 {
-		for i := 0; i < len(c); i++ {
-			switch c[i].(type) {
-			case decimal.Decimal:
-				sum = sum.Add(c[i].(decimal.Decimal))
-			default:
-				continue
+		if n, ok := c.value.([]decimal.Decimal); ok {
+			for i := 0; i < len(n); i++ {
+				sum = sum.Add(n[i])
 			}
 		}
 	} else {
-		for i := 0; i < len(c); i++ {
-			switch c[i].(type) {
-			case map[string]interface{}:
-				sum = sum.Add(NewDecimalFromInterface(c[i].(map[string]interface{})[key[0]]))
-			default:
-				continue
+		if n, ok := c.value.([]map[string]interface{}); ok {
+			for i := 0; i < len(n); i++ {
+				sum = sum.Add(NewDecimalFromInterface(n[i][key[0]]))
 			}
 		}
 	}
@@ -89,36 +110,27 @@ func (c Collection) Sum(key ...string) decimal.Decimal {
 
 func (c Collection) Min(key ...string) decimal.Decimal {
 
-	var (
-		smallest = decimal.New(0, 0)
-		ok       bool
-	)
+	var smallest = decimal.New(0, 0)
 
 	if len(key) == 0 {
-
-		var v decimal.Decimal
-
-		for i := 0; i < len(c); i++ {
-			if v, ok = c[i].(decimal.Decimal); ok {
+		if n, ok := c.value.([]decimal.Decimal); ok {
+			for i := 0; i < len(n); i++ {
 				if i == 0 {
-					smallest = v
+					smallest = n[i]
 					continue
 				}
-				if smallest.GreaterThan(v) {
-					smallest = v
+				if smallest.GreaterThan(n[i]) {
+					smallest = n[i]
 				}
 			}
 		}
 	} else {
 
-		var (
-			m      map[string]interface{}
-			number decimal.Decimal
-		)
+		var number decimal.Decimal
 
-		for i := 0; i < len(c); i++ {
-			if m, ok = c[i].(map[string]interface{}); ok {
-				number = NewDecimalFromInterface(m[key[0]])
+		if n, ok := c.value.([]map[string]interface{}); ok {
+			for i := 0; i < len(n); i++ {
+				number = NewDecimalFromInterface(n[i][key[0]])
 				if i == 0 {
 					smallest = number
 					continue
@@ -135,36 +147,27 @@ func (c Collection) Min(key ...string) decimal.Decimal {
 
 func (c Collection) Max(key ...string) decimal.Decimal {
 
-	var (
-		biggest = decimal.New(0, 0)
-		ok      bool
-	)
+	var biggest = decimal.New(0, 0)
 
 	if len(key) == 0 {
-
-		var v decimal.Decimal
-
-		for i := 0; i < len(c); i++ {
-			if v, ok = c[i].(decimal.Decimal); ok {
+		if n, ok := c.value.([]decimal.Decimal); ok {
+			for i := 0; i < len(n); i++ {
 				if i == 0 {
-					biggest = v
+					biggest = n[i]
 					continue
 				}
-				if biggest.LessThan(v) {
-					biggest = v
+				if biggest.LessThan(n[i]) {
+					biggest = n[i]
 				}
 			}
 		}
 	} else {
 
-		var (
-			m      map[string]interface{}
-			number decimal.Decimal
-		)
+		var number decimal.Decimal
 
-		for i := 0; i < len(c); i++ {
-			if m, ok = c[i].(map[string]interface{}); ok {
-				number = NewDecimalFromInterface(m[key[0]])
+		if n, ok := c.value.([]map[string]interface{}); ok {
+			for i := 0; i < len(n); i++ {
+				number = NewDecimalFromInterface(n[i][key[0]])
 				if i == 0 {
 					biggest = number
 					continue
@@ -177,6 +180,53 @@ func (c Collection) Max(key ...string) decimal.Decimal {
 	}
 
 	return biggest
+}
+
+func (c Collection) Join(delimiter string) string {
+	s := ""
+
+	if n, ok := c.value.([]string); ok {
+		for i := 0; i < len(n); i++ {
+			if i != len(n)-1 {
+				s += n[i] + delimiter
+			} else {
+				s += n[i]
+			}
+		}
+	} else if n, ok := c.value.([]decimal.Decimal); ok {
+		for i := 0; i < len(n); i++ {
+			if i != len(n)-1 {
+				s += n[i].String() + delimiter
+			} else {
+				s += n[i].String()
+			}
+		}
+	}
+
+	return s
+}
+
+func (c Collection) Combine(value []interface{}) Collection {
+	var (
+		m      = make(map[string]interface{}, 0)
+		length = c.length
+	)
+
+	if length > len(value) {
+		length = len(value)
+	}
+
+	if n, ok := c.value.([]string); ok {
+		for i := 0; i < length; i++ {
+			m[n[i]] = value[i]
+		}
+	}
+
+	return Collect(m)
+}
+
+func (c Collection) Count() int {
+	return c.length
 }
 
 func (c Collection) Mode(key ...string) []interface{} {
@@ -217,6 +267,14 @@ func (c Collection) Take(num int) Collection {
 
 func (c Collection) ToJson() string {
 	panic("implement it")
+}
+
+func (c Collection) ToMap() map[string]interface{} {
+	if m, ok := c.value.(map[string]interface{}); ok {
+		return m
+	} else {
+		return map[string]interface{}{}
+	}
 }
 
 func (c Collection) Where(key string, value interface{}) Collection {
