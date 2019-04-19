628e392ad4c605d3cc9066c573b9ec082ae61913
brucegao77 <brucegxs@gmail.com>
2019-04-19 15:57:14 +0800

    optimization of Collapse()

--- collection/muti_dimensional_array_collection.go
+++ collection/muti_dimensional_array_collection.go
@@ -9,25 +9,17 @@ func (c MultiDimensionalArrayCollection) Collapse() Collection {
 	if len(c.value[0]) == 0 {
 		return Collect([]interface{}{})
 	}
-
-	// length = (len(outer_slice)-1) * len(inner_slice) + len(last_inner_slice)
-	outer := len(c.value) - 1
-	inner := len(c.value[0])
-	last := len(c.value[len(c.value)-1])
-	length := outer*inner + last
-	d := make([]interface{}, length)
-
-	// range outer_slice, and inner_slice except last one
-	for i := 0; i < outer; i++ {
-		for j := 0; j < inner; j++ {
-			d[i*inner+j] = c.value[i][j]
-		}
+	length := 0
+	for _, v := range c.value {
+		length += len(v)
 	}
 
-	// range last_inner slice
-	if last != 0 {
-		for i := 0; i < last; i++ {
-			d[outer*inner+i] = c.value[len(c.value)-1][i]
+	d := make([]interface{}, length)
+	index := 0
+	for _, innerSlice := range c.value {
+		for _, v := range innerSlice {
+			d[index] = v
+			index++
 		}
 	}
 
