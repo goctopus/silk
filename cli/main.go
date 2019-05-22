1ea2a943bf76eab07f6a32a52d77c2a559d70940
cg33 <chg80333@163.com>
2019-05-22 17:42:42 +0800

    cli template and framework

--- cli/main.go
+++ cli/main.go
@@ -1,5 +1,130 @@
 package main
 
-func main()  {
-	
+import (
+	"errors"
+	"fmt"
+	"github.com/schollz/progressbar/v2"
+	"time"
+
+	"github.com/manifoldco/promptui"
+)
+
+func main() {
+
+	fmt.Println()
+	fmt.Println("[======database driver======]")
+	fmt.Println()
+
+	promptSelect := promptui.Select{
+		Label: "choose a driver",
+		Items: []string{"mysql", "mssql", "postgres", "sqlite"},
+	}
+
+	_, _, err := promptSelect.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Println()
+	fmt.Println("[======database config======]")
+	fmt.Println()
+
+	prompt("address ")
+	prompt("username ")
+	promptPassword("password ")
+
+	fmt.Println()
+	fmt.Println("[===========tables===========]")
+	fmt.Println()
+
+	tables := []string{"post", "users", "article", "audio", "[finish]"}
+	var value string
+	for tables[0] != "[finish]" && value != "[finish]" {
+		value = selects(tables)
+		tables = removeItem(tables, value)
+	}
+
+	fmt.Println()
+	fmt.Println("[=========generating=========]")
+	fmt.Println()
+
+	bar := progressbar.New(100)
+	for i := 0; i < 100; i++ {
+		_ = bar.Add(1)
+		time.Sleep(10 * time.Millisecond)
+	}
+
+	fmt.Println()
+	fmt.Println()
+}
+
+func prompt(label string) {
+	validate := func(input string) error {
+		if len(input) < 3 {
+			return errors.New("Username must have more than 3 characters")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+	}
+
+	_, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+}
+
+func promptPassword(label string) {
+	validate := func(input string) error {
+		if len(input) < 3 {
+			return errors.New("Username must have more than 3 characters")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+		Mask:     '*',
+	}
+
+	_, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+}
+
+func selects(tables []string) string {
+	promptSelect := promptui.Select{
+		Label: "choose table to generate",
+		Items: tables,
+	}
+
+	_, result, err := promptSelect.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return ""
+	}
+
+	return result
+}
+
+func removeItem(tables []string, table string) []string {
+	index := 0
+	for i := 0; i < len(tables); i++ {
+		if tables[i] == table {
+			index = i
+		}
+	}
+	return append(tables[:index], tables[index+1:]...)
 }
--- go.mod
+++ go.mod
@@ -8,7 +8,13 @@ require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/lib/pq v1.0.0
 	github.com/magiconair/properties v1.8.0
+	github.com/manifoldco/promptui v0.3.2
 	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/nicksnyder/go-i18n v1.10.0 // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/schollz/progressbar/v2 v2.13.0 // indirect
 	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24
+	github.com/stretchr/objx v0.2.0 // indirect
 	golang.org/x/crypto v0.0.0-20190313024323-a1f597ede03a // indirect
+	gopkg.in/alecthomas/kingpin.v3-unstable v3.0.0-20180810215634-df19058c872c // indirect
 )
